3869a83601509a57e36ad8af514736ac910c57b0
chrome-dino <42478360+chrome-dino@users.noreply.github.com>
2022-05-26 15:43:50 -0400

    Update __main__.py

--- src/mysql_enumerator/__main__.py
+++ src/mysql_enumerator/__main__.py
@@ -7,14 +7,14 @@ def main():
 
     # cmd line args
     
-    parser.add_argument("-db", "--hostname", help="database",required=True)
-    parser.add_argument("-u", "--username", help="User name",required=True)
-    parser.add_argument("-p", "--password", help="Password",required=True)
-    parser.add_argument("-port", "--port", help="port", type=int, required=False)
-    parser.add_argument("-s", "--schema", help="schema",required='-t' in sys.argv)
-    parser.add_argument("-t", "--table", help="table", required='-s' in sys.argv)
-    parser.add_argument("-a", "--admin", help="admin mode", action=argparse.BooleanOptionalAction)
-    parser.add_argument("-v", "--verbose", help="verbose", action=argparse.BooleanOptionalAction)
+    parser.add_argument("-db", "--hostname", help="IP address or hostname of the target database",required=True)
+    parser.add_argument("-u", "--username", help="Login username",required=True)
+    parser.add_argument("-p", "--password", help="Login Password",required=True)
+    parser.add_argument("-port", "--port", help="Port number (Defaults to 3306)", type=int, required=False)
+    parser.add_argument("-s", "--schema", help="Name of the schema to be used in table extraction mode. Requires the table option",required='-t' in sys.argv)
+    parser.add_argument("-t", "--table", help="Name of the table to be used in table extraction mode. Requires the schema option", required='-s' in sys.argv)
+    parser.add_argument("-a", "--admin", help="Enable admin mode to extract database user info. Requires admin credentials", action=argparse.BooleanOptionalAction)
+    parser.add_argument("-v", "--verbose", help="List additional details in the user report", action=argparse.BooleanOptionalAction)
 
     args = parser.parse_args()
     
