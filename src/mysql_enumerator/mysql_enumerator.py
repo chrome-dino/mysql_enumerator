5af01b288cf0c07378d4366a14ddd6ba70f103bd
chrome-dino <42478360+chrome-dino@users.noreply.github.com>
2022-05-26 00:00:04 -0400

    Update mysql_enumerator.py

--- src/mysql_enumerator/mysql_enumerator.py
+++ src/mysql_enumerator/mysql_enumerator.py
@@ -1,20 +1,32 @@
 import os.path
 import mysql_enumerator.mysql_enumerator_constants
-
+import pandas as pd
+import pymysql
 
 class MySqlEnumerator():
 
-    def __init__(self):
-        print('!')
+    def __init__(self, hostname='', username='', password='', port=3306, schema=None, table=None, admin=False, verbose=False):
+        # Intialize member variables
+        self.hostname = hostname
+        self.username = username
+        self.password = password
+        self.schema = schema
+        self.port = port
+        self.table = table
+        self.admin = admin
+        self.verbose = verbose
+        self.connection = None
+        
 
-    def exfil_table(self, table, schema, cursor):
-        sql = "SELECT * FROM " + schema + "." + table
+
+    def exfil_table(self, table, cursor):
+        sql = "SELECT * FROM " + self.schema + "." + table
         cursor.execute(sql)
         result = cursor.fetchall()
         return result
 
     # collect info on all tables in db
-    def db_report(self, cursor, admin):
+    def db_report(self, cursor):
         final = {}
         db_layout = []
         # collect schemas
@@ -27,12 +39,14 @@ class MySqlEnumerator():
             tables = []
             schema = x['schema_name']
             sql = "SELECT table_name FROM information_schema.tables WHERE table_schema = '" + schema
+            cursor.execute(sql)
             table_query = cursor.fetchall()
 
             # collect columns for each table
             for y in table_query:
                 table = y['table_name']
                 sql = "SELECT COLUMN_NAME,DATA_TYPE FROM information_schema.COLUMNS WHERE table_name = '" + table
+                cursor.execute(sql)
                 columns = cursor.fetchall()
                 cols = []
 
@@ -40,136 +54,129 @@ class MySqlEnumerator():
                     cols.append({'column': z['COLUMN_NAME'], 'data_type': z['DATA_TYPE']})
                 tables.append({'table': table, 'columns': cols})
 
-        db_layout.append({'schema': schema, 'tables': tables})
+            db_layout.append({'schema': schema, 'tables': tables})
 
         final['db_layout'] = db_layout
 
         # if running in admin mode collect user data
-        if admin:
+        if self.admin:
             sql = "SELECT * FROM user"
-        users = cursor.fetchall()
-        final['users'] = users
+            cursor.execute(sql)
+            users = cursor.fetchall()
+            final['users'] = users
 
         return final
 
 
-def run():
-    if not args.port:
-        args.port = 3306
+    def run(self):
 
-    # admin mode defaults to False if no option is provided
-
-
-    if args.admin:
-        admin = True
-    else:
-        admin = False
-    
         # connect to the databse
-    connection = pymysql.connect(host=args.hostname,
-                                 user=args.username,
-                                 password=args.password,
-                                 # db=args.schema,
-                                 charset='utf8mb4',
-                                 port=args.port,
-                                 cursorclass=pymysql.cursors.DictCursor)
-    
-    try:
-        with connection.cursor() as cursor:
-            # if table and schema provided output content of table to an excel file
-            if args.t:
-                for x in str(args.table).split(','):
-                    results = exfil_table(x, args.schema, cursor)
-                    df = pd.DataFrame(results)
-                    df.to_excel(args.t + '.xlsx')
-            else:
-                # if no table is provided print out details about all tables in the database
-                results = db_report(cursor, admin)
-                for schema in results['schema']:
-                    print('SCHEMA: ' + schema['schema'])
-                    print('---------------------------------------------------\n')
-                    for table in schema['table']:
-                        print('\tTABLE: ' + table)
-                        for column in table['column']:
-                            print('\t\t' + column['column'] + ": " + column['data_type'])
-                        print('\n\n')
-    
-                # if running in admin mode print out user info
-                if admin:
-                    df = pd.DataFrame(results['users'])
-                    df.to_excel('users.xlsx')
-                    print('USERS')
-                    print('---------------------------------------------------\n')
-                    for user in results['users']:
-                        print('\tHOST: ' + user['Host'])
-                        print('\tUSER: ' + user['User'])
-                        print('\tPASSWORD: ' + user['Password'] + '  EXPIRED: ' + user['password_expired'])
-    
-                        # print out user privs. Uncomment to display more privs
-                        print('\tPRIVILEGES:')
-                        if user['Select_priv'] == 'Y':
-                            print('\t\tSELECT')
-                        if user['Inser_priv'] == 'Y':
-                            print('\t\tINSERT')
-                        if user['Update_priv'] == 'Y':
-                            print('\t\tUPDATE')
-                        if user['Delete_priv'] == 'Y':
-                            print('\t\tDELETE')
-                        if user['Create_priv'] == 'Y':
-                            print('\t\tCREATE')
-                        if user['Drop_priv'] == 'Y':
-                            print('\t\tDROP')
-                        # if user['Reload_priv'] == 'Y':
-                        #    print('\t\tRELOAD')
-                        # if user['Shutdown_priv'] == 'Y':
-                        #    print('\t\tSHUTDOWN')
-                        # if user['Process_priv'] == 'Y':
-                        #    print('\t\tPROCESS')
-                        if user['File_priv'] == 'Y':
-                            print('\t\tFILE')
-                        if user['Grant_priv'] == 'Y':
-                            print('\t\tGRANT')
-                        # if user['References_priv'] == 'Y':
-                        #    print('\t\tREFERENCES')
-                        # if user['Index_priv'] == 'Y':
-                        #    print('\t\tINDEX')
-                        # if user['Alter_priv'] == 'Y':
-                        #    print('\t\tALTER')
-                        # if user['Show_db_priv'] == 'Y':
-                        #    print('\t\tSHOW_DB')
-                        if user['Super_priv'] == 'Y':
-                            print('\t\tSUPER')
-                        # if user['Create_tmp_table_priv'] == 'Y':
-                        #    print('\t\tCREATE_TMP_TABLE')
-                        # if user['Lock_tables_priv'] == 'Y':
-                        #    print('\t\tLOCK_TABLES')
-                        # if user['Execute_priv'] == 'Y':
-                        #    print('\t\tEXECUTE')
-                        # if user['Repl_slave_priv'] == 'Y':
-                        #    print('\t\tREPL_SLAVE')
-                        # if user['Create_view_priv'] == 'Y':
-                        #    print('\t\tCREATE_VIEW')
-                        # if user['Show_view_priv'] == 'Y':
-                        #    print('\t\tSHOW_VIEW')
-                        # if user['Create_routine_priv'] == 'Y':
-                        #    print('\t\tCREATE_ROUTINE')
-                        # if user['Alter_routine_priv'] == 'Y':
-                        #    print('\t\tALTER_ROUTINE')
-                        if user['Create_user_priv'] == 'Y':
-                            print('\t\tCREATE_USER')
-                        # if user['Event_priv'] == 'Y':
-                        #    print('\t\tEVENT')
-                        # if user['Trigger_priv'] == 'Y':
-                        #    print('\t\tTRIGGER')
-                        # if user['Create_tablespace_priv'] == 'Y':
-                        #    print('\t\tCREATE_TABLESPACE')
-                        # if user['Delete_history_priv'] == 'Y':
-                        #    print('\t\tDELETE_HISTORY')
-    
-    
-    except pymysql.err.OperationalError:
-        print('Unable to make a connection to the mysql database')
-    finally:
-        connection.close()
-    
-    
+        self.connection = pymysql.connect(host=self.hostname,
+                                     user=self.username,
+                                     password=self.password,
+                                     # db=args.schema,
+                                     charset='utf8mb4',
+                                     port=self.port,
+                                     cursorclass=pymysql.cursors.DictCursor)
+
+        try:
+            with self.connection.cursor() as cursor:
+                # if table and schema provided output content of table to an excel file
+                if self.table:
+                    for x in str(self.table).split(','):
+                        results = self.exfil_table(x, cursor)
+                        df = pd.DataFrame(results)
+                        df.to_excel(x + '.xlsx')
+                else:
+                    # if no table is provided print out details about all tables in the database
+                    results = self.db_report(cursor)
+                    for schema in results['schema']:
+                        print('SCHEMA: ' + schema['schema'])
+                        print('---------------------------------------------------\n')
+                        for table in schema['table']:
+                            print('\tTABLE: ' + table)
+                            for column in table['column']:
+                                print('\t\t' + column['column'] + ": " + column['data_type'])
+                            print('\n\n')
+
+                    # if running in admin mode print out user info
+                    if self.admin:
+                        df = pd.DataFrame(results['users'])
+                        df.to_excel('users.xlsx')
+                        print('USERS')
+                        print('---------------------------------------------------\n')
+                        for user in results['users']:
+                            print('\tHOST: ' + user['Host'])
+                            print('\tUSER: ' + user['User'])
+                            print('\tPASSWORD: ' + user['Password'] + '  EXPIRED: ' + user['password_expired'])
+
+                            # print out user privs
+                            print('\tPRIVILEGES:')
+                            if user['Select_priv'] == 'Y':
+                                print('\t\tSELECT')
+                            if user['Inser_priv'] == 'Y':
+                                print('\t\tINSERT')
+                            if user['Update_priv'] == 'Y':
+                                print('\t\tUPDATE')
+                            if user['Delete_priv'] == 'Y':
+                                print('\t\tDELETE')
+                            if user['Create_priv'] == 'Y':
+                                print('\t\tCREATE')
+                            if user['Drop_priv'] == 'Y':
+                                print('\t\tDROP')
+                            if user['File_priv'] == 'Y':
+                                print('\t\tFILE')
+                            if user['Grant_priv'] == 'Y':
+                                print('\t\tGRANT')
+                            if user['Super_priv'] == 'Y':
+                                print('\t\tSUPER')
+                            if user['Create_user_priv'] == 'Y':
+                                print('\t\tCREATE_USER')
+                            
+                            if self.verbose:
+                                if user['Reload_priv'] == 'Y':
+                                   print('\t\tRELOAD')
+                                if user['Shutdown_priv'] == 'Y':
+                                   print('\t\tSHUTDOWN')
+                                if user['Process_priv'] == 'Y':
+                                   print('\t\tPROCESS')
+                                if user['References_priv'] == 'Y':
+                                   print('\t\tREFERENCES')
+                                if user['Index_priv'] == 'Y':
+                                   print('\t\tINDEX')
+                                if user['Alter_priv'] == 'Y':
+                                   print('\t\tALTER')
+                                if user['Show_db_priv'] == 'Y':
+                                   print('\t\tSHOW_DB')
+                                if user['Create_tmp_table_priv'] == 'Y':
+                                   print('\t\tCREATE_TMP_TABLE')
+                                if user['Lock_tables_priv'] == 'Y':
+                                   print('\t\tLOCK_TABLES')
+                                if user['Execute_priv'] == 'Y':
+                                   print('\t\tEXECUTE')
+                                if user['Repl_slave_priv'] == 'Y':
+                                   print('\t\tREPL_SLAVE')
+                                if user['Create_view_priv'] == 'Y':
+                                   print('\t\tCREATE_VIEW')
+                                if user['Show_view_priv'] == 'Y':
+                                   print('\t\tSHOW_VIEW')
+                                if user['Create_routine_priv'] == 'Y':
+                                   print('\t\tCREATE_ROUTINE')
+                                if user['Alter_routine_priv'] == 'Y':
+                                   print('\t\tALTER_ROUTINE')
+                                if user['Event_priv'] == 'Y':
+                                   print('\t\tEVENT')
+                                if user['Trigger_priv'] == 'Y':
+                                   print('\t\tTRIGGER')
+                                if user['Create_tablespace_priv'] == 'Y':
+                                   print('\t\tCREATE_TABLESPACE')
+                                if user['Delete_history_priv'] == 'Y':
+                                   print('\t\tDELETE_HISTORY')
+
+
+        except pymysql.err.OperationalError:
+            print('Unable to make a connection to the mysql database')
+        finally:
+            self.connection.close()
+
+
